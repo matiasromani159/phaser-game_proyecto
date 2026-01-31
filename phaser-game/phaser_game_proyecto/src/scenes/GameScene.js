dae2092c8494560a20e4e813df81904d4ea9efda
solblanco733 <williamscordovabryan@gmail.com>
2026-01-31 12:07:43 +0100

    Hice todo
    Hice el ersoanej

--- phaser-game/phaser_game_proyecto/src/scenes/GameScene.js
+++ phaser-game/phaser_game_proyecto/src/scenes/GameScene.js
@@ -6,6 +6,8 @@ export default class GameScene extends Phaser.Scene {
     }
 
     preload() {
+//bryan hizo todo
+
         this.load.audio('snd_board', '/src/assets/sounds/tenna_island.ogg');
 
         this.load.image('healthbar', '/src/assets/sprites/spr_hp_bar.png');
