2c97e4aa55ade8b808d81085b7ec32ce372e14d0
matiasromani159 <matiasromani159@gmail.com>
2026-02-19 20:27:00 +0100

    Mapa
    Mapa con tiled falta escalar

--- phaser-game/phaser_game_proyecto/src/assets/tiles/tipe.tsx
+++ phaser-game/phaser_game_proyecto/src/assets/tiles/tipe.tsx
@@ -1,4 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="tipe" tilewidth="36" tileheight="36" tilecount="930" columns="30">
  <image source="tipe.png" width="1080" height="1116"/>
+ <tile id="90">
+  <animation>
+   <frame tileid="90" duration="400"/>
+   <frame tileid="91" duration="400"/>
+   <frame tileid="92" duration="400"/>
+   <frame tileid="93" duration="400"/>
+  </animation>
+ </tile>
 </tileset>
